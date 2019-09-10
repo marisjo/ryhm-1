a0b89dd949a7c26acc3b87bbf2aa3f79ce44d0f9
Andrus Rinde <andrusrinde@users.noreply.github.com>
2019-09-10 11:09:15 +0300

    Esimene üleslaadimine

---
+++ 02tund/myindex.php
@@ -0,0 +1,22 @@
+<?php
+  //echo "Hei!";
+  phpversion();
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Andrus Rinde programmeerib veebi</title>
+
+</head>
+<body>
+  <h1>Andrus Rinde, veebiprogrammeerimine</h1>
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <hr>
+  <p>PHP serveri konfiguratsiooni infot näeb <a href="serverinfo.php">siin</a>.</p>
+  <p>Minu esimene programmeeritud leht on <a href="testpage.php">siin</a>.</p>
+</body>
+</html>
+
+
+
---
+++ 02tund/testpage.php
@@ -0,0 +1,28 @@
+<?php
+  $userName = "Andrus van der Rinde";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+    echo $userName;
+  ?>
+   programmeerib veebi</title>
+
+</head>
+<body>
+  <?php
+    echo "<h1>" .$userName .", veebiprogrammeerimine</h1>";
+  ?>
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+</body>
+</html>
+
+
+
+
+
+
+
