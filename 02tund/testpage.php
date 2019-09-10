87ea5a84d4ebd8eb26049598f41cf5c960f58196
Andrus Rinde <andrusrinde@users.noreply.github.com>
2019-09-10 11:34:09 +0300

    lisatud kellaaja kontrollimine

--- 02tund/testpage.php
+++ 02tund/testpage.php
@@ -1,5 +1,12 @@
 <?php
   $userName = "Andrus van der Rinde";
+  $fullTimeNow = date("d.m.Y H:i:s");
+  $hourNow = date("H");
+  $partOfDay = "hägune aeg";
+  
+  if($hourNow < 8){
+	$partOfDay = "hommik";
+  }
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -17,6 +24,10 @@
     echo "<h1>" .$userName .", veebiprogrammeerimine</h1>";
   ?>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <hr>
+  <?php
+    echo "<p>Lehe avamise hetkel oli aeg: " .$fullTimeNow .", " .$partOfDay .".</p>";
+  ?>
 </body>
 </html>
 
